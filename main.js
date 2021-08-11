47fdc4e1f6e02569ab8d246aad81e6300d7918cc
jangyeonjae <80942823+jangyeonjae@users.noreply.github.com>
2021-08-11 15:11:41 +0900

    인덱스

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
+
+/*play / pause*/
 let clicked = 0;
-        function play(){
-            let playPause = document.getElementById("play-pause");
+    function play(){
+        let playPause = document.getElementById("play-pause");
             
-            if(clicked){
+        if(clicked){
                 playPause.className ="play";
                 clicked = 0;
                 audio.play();
-            }else{
+        }else{
                 playPause.className ="pause";
                 clicked = 1;
                 audio.pause();
-            }
         }
- 
+    }
+ /*progress bar*/
